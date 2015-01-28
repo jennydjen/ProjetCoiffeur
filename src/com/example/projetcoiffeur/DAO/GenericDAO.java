973fc678097f32c9e6db9416a7671942ff6e12f7
Thibaut Chaumont <chaumont.thibaut@gmail.com>
2015-01-28 16:48:35 +0100

    [DEV:1004]Création d'une interface générique pour les DAO Ajout du persistance.xml

---
+++ src/com/example/projetcoiffeur/DAO/GenericDAO.java
@@ -0,0 +1,31 @@
+package com.example.projetcoiffeur.DAO;
+
+public interface GenericDAO<T> {
+	/**
+	 * Créer une valeur dans la table
+	 * @param t
+	 * @return
+	 */
+	public T create(T t);
+	/**
+	 * Mettre à jour la valeur dans la table 
+	 * @param t
+	 * @return
+	 */
+	public T update(T t);
+	/**
+	 * Trouver un champ dans la table
+	 * @param id
+	 * @return
+	 */
+	public T find(long id);
+	/**
+	 * Supprimer un champ dans la table
+	 * @param id
+	 * @return
+	 */
+	public T delete(long id);
+	
+	
+
+}
