b84eb8415c82fcb6ab538fe999b77b0aaceea127
LAINE Guillaume <laine.guillaume@hotmail.fr>
2015-01-28 16:02:39 +0100

    [DEV:1011] Création entity Service

---
+++ src/com/example/projetcoiffeur/entity/Service.java
@@ -0,0 +1,39 @@
+package com.example.projetcoiffeur.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import com.example.projetcoiffeur.entity.enumeration.TypeCompte;
+
+@Entity
+public class Service {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	private String description;
+	private double prix;
+	
+	@Enumerated(EnumType.ORDINAL)
+	private TypeCompte type_service;
+	
+	
+	public Long getId() {
+		return id;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public double getPrix() {
+		return prix;
+	}
+	public void setPrix(double prix) {
+		this.prix = prix;
+	}
+}
