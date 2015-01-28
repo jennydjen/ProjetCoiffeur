e30e40883f75f38afd5566c9280db6401e219e2b
Thibaut Chaumont <chaumont.thibaut@gmail.com>
2015-01-28 15:42:16 +0100

    [DEV:1002]Création l'entity Client en java

---
+++ src/com/example/projetcoiffeur/entity/Client.java
@@ -0,0 +1,59 @@
+package com.example.projetcoiffeur.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Client {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private long id;
+	private String nom;
+	private String prenom;
+	
+	private String adresse;
+	private int telephone;
+	private boolean inactif;
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public String getPrenom() {
+		return prenom;
+	}
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+	public String getAdresse() {
+		return adresse;
+	}
+	public void setAdresse(String adresse) {
+		this.adresse = adresse;
+	}
+	public int getTelephone() {
+		return telephone;
+	}
+	public void setTelephone(int telephone) {
+		this.telephone = telephone;
+	}
+	public boolean isInactif() {
+		return inactif;
+	}
+	public void setInactif(boolean inactif) {
+		this.inactif = inactif;
+	}
+	
+	
+
+}
