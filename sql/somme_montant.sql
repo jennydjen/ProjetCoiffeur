966e50cda221c0e4312883c22cd8ea480da2c025
Jennifer Chaignepain <chaignepain.jennifer@gmail.com>
2015-01-28 17:01:04 +0100

    [DEV : 1040] Creation du fichier sql de somme_montant

---
+++ sql/somme_montant.sql
@@ -0,0 +1,8 @@
+create table somme_montant(
+	id bigint not null generated always AS IDENTITY constraint id_pksomme_montant Primary key,
+	id_intervention bigint,
+	id_service bigint,
+	prix double, 
+	CONSTRAINT fk_intervention FOREIGN KEY (id_intervention) REFERENCES Intervention(id),
+	CONSTRAINT fk_service FOREIGN KEY (id_service) REFERENCES Service(id));
+)
