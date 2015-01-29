fc23412f1662b13e18c0e0db5b0d796ecbe4f767
Jennifer Chaignepain <chaignepain.jennifer@gmail.com>
2015-01-29 15:16:00 +0100

    Insert into client and service

---
+++ sql/InsertInto.sql
@@ -0,0 +1,20 @@
+INSERT INTO CLIENT (name , prenom, adresse, telephone, inactif)
+	VALUES ('chaignepain', 'jennifer', 'mon adresse', '001122334455',0),
+	('chaumont', 'thibaut', 'mon adresse', '001122334455',0),
+	('delechat', 'william', 'mon adresse', '001122334455',0),
+	('laine', 'guillaume', 'mon adresse', '001122334455',0),
+	('natulique', 'thierry', 'mon adresse', '001122334455',0),
+	('clooney', 'georges', 'mon adresse', '001122334455',0),
+	('morel', 'marcel', 'mon adresse', '001122334455',0),
+	('mars', 'bruno', 'mon adresse', '001122334455',0),
+	('hawgins', 'stephane', 'mon adresse', '001122334455',0),
+	('de la jungle', 'geoges', 'mon adresse', '001122334455',0);
+
+INSERT INTO SERVICE (description, prix, type_service)
+	VALUES('Shampooing - Coupe homme', 14, 703),
+	('Shampooing - Coupe femme - Brushing', 23, 703),
+	('Couleur - coupe brushing', 41, 703),
+	('Mèches - Shampooing Coupe Brushing', 45, 703),
+	('Coupe fille - 14 ans', 13, 703),
+	('Coupe garçon - 14 ans', 10, 703),
+	('Vente produits', 0, 701);
