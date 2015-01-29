a8fe2b1627cc3d6c9a8a6d55343b3f04af9a6cd7
LAINE Guillaume <laine.guillaume@hotmail.fr>
2015-01-29 11:39:17 +0100

    [conflit]renvoi de service DAO nettoyé

---
+++ src/com/example/projetcoiffeur/DAO/ServiceDAO.java
@@ -0,0 +1,37 @@
+package com.example.projetcoiffeur.DAO;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import com.example.projetcoiffeur.DAO.interfaces.GenericDAO;
+import com.example.projetcoiffeur.entity.Service;
+
+public class ServiceDAO implements GenericDAO<Service>{
+
+	@Override
+	public Service create(Service t) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Service update(Service t) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Service find(long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void delete(long id) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
